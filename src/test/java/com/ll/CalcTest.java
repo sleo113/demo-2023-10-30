3b80b01d6a60a7f6e09a0d79899819d36353b215
JeUk Park <qkrwpdnr4531@naver.com>
2023-10-30 10:31:54 +0900

    Setting + Add Calc class + Add CalcTest

---
+++ src/test/java/com/ll/CalcTest.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalcTest {
+    @Test
+    @DisplayName("Calc에 run 메서드는 int를 리턴한다.")
+
+    void t1(){
+            int rs = Calc.run("");
+    }
+}
